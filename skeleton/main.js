e33edd274750eeb3f1aef5eef6a2ac7e68a42800
Fady Samuel <fsamuel@chromium.org>
2012-12-06 15:15:11 -0500

    Added some skeleton code

---
+++ skeleton/main.js
@@ -0,0 +1,12 @@
+  /**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('webview.html', {
+    'width': 1024,
+    'height': 768
+  });
+});
---
+++ skeleton/webview.js
@@ -0,0 +1,4 @@
+onload = function() {
+  var webview = document.querySelector('webview');
+  webview.src = "http://www.google.com";
+};
